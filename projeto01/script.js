65e6405e3ce518913bd51ea4184958b484fe43ec
Flavio dos Santos <flaviovorthrox@yahoo.com.br>
2021-07-20 21:01:02 -0300

    playComposition, 31min de aula #projeto01

--- projeto01/script.js
+++ projeto01/script.js
@@ -1,8 +1,22 @@
+// eventos
+
 document.body.addEventListener('keyup', (event) => {
     playSound(event.code.toLowerCase());
 });
 
 
+document.querySelector('.composer button').addEventListener('click', () => {
+    let song = document.querySelector('#input').value;
+
+    if (song !== '') {
+        let songArray = song.split('');
+        playComposition(songArray);
+    }
+
+});
+
+
+// funções
 function playSound(sound) {
     let audioElement = document.querySelector(`#s_${sound}`);
     let keyElement = document.querySelector(`div[data-key="${sound}"]`);
@@ -19,4 +33,11 @@ function playSound(sound) {
             keyElement.classList.remove('active');
         }, 300);
     }
-};
+}
+
+
+function playComposition(songArray) {
+    for (let songItem of songArray) {
+        playSound(`key${songItem}`);
+    }
+}
