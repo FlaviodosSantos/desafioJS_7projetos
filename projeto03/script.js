7055763dce0c35ee9a5bebb935cfbd7312d811ab
Flavio dos Santos <flaviovorthrox@yahoo.com.br>
2021-07-22 22:24:23 -0300

    add API openWheather #p03

---
+++ projeto03/script.js
@@ -0,0 +1,19 @@
+document.querySelector('.busca').addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+    let input = document.querySelector('#searchInput').value;
+
+    if (input !== '') {
+        showWarning('Carregando ...');
+
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(input)}&appid=f04fbc6da3aa9d738c8367e088e07652&units=metric&lang=pt_br`;
+
+        let results = await fetch(url);
+        let json = await results.json();
+    }
+});
+
+
+function showWarning(msg) {
+    document.querySelector('.aviso').innerHTML = msg;
+}
